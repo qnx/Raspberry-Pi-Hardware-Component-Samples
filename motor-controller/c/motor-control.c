8cffbc44a9d8eb87dc245db9ec1aee7320804cec
Muhammad Ibrahim <muibrahim@qnx.com>
2025-02-12 18:17:08 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: John Hanam (QNX) <john@blackberry.com>

--- motor-controller/c/motor-control.c
+++ motor-controller/c/motor-control.c
@@ -53,7 +53,7 @@ int main(void) {
     sleep(2);
 
     // Move the motors in a backward direction
-    // To Move the motors backward, the negative terminal of the left and right motors needs to be set
+    // To move the motors backward, the negative terminal of the left and right motors needs to be set
     // Likewise the positive terminal of the left and right motors needs to be cleared
     rpi_gpio_set(MOTOR_RP);
     rpi_gpio_clear(MOTOR_RP);
