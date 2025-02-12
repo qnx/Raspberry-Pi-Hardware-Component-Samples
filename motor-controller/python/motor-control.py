84c6498904d0823e18ba79d6b33755c18ff5f548
Muhammad Ibrahim <muibrahim@qnx.com>
2025-02-12 18:19:34 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: John Hanam (QNX) <john@blackberry.com>

--- motor-controller/python/motor-control.py
+++ motor-controller/python/motor-control.py
@@ -5,7 +5,7 @@ import rpi_gpio as GPIO
 MOTOR_EN_PIN = 26 # GPIO pin 26, Enable pin is used to turn on and off the motors
 MOTOR_RP = 10 # GPIO pin 10, Controls the input to the positive terminal of the right motor
 MOTOR_RN = 9 # GPIO pin 9, Controls the input to the negative terminal of the right motor
-MOTOR_LP = 8 # GPIO pin 8, Controls the input to the positive terminal off the left motor
+MOTOR_LP = 8 # GPIO pin 8, Controls the input to the positive terminal of the left motor
 MOTOR_LN = 11 # GPIO pin 11, Controls the input to the negative terminal of the left motor
 
 # Initialize all of the defined pins to be outputs
