99eb294360649e6f186a878ec201a48a468cec58
Niraj Patel <nirajpatel@BB585107940.rim.net>
2025-03-11 09:55:52 -0400

    Adding Apache headers & Removing unwanted source files

--- hc-sr04_ultrasonic_sensor/c/src/Ultrasonic.c
+++ hc-sr04_ultrasonic_sensor/c/src/Ultrasonic.c
@@ -1,12 +1,26 @@
 /**
+* Copyright (c) 2025, BlackBerry Limited. All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
 * @file Ultrasonic.c
-* @brief HC‑SR04 Ultrasonic Sensor Example for QNX on the Trilobot.
+* @brief HC‑SR04 Ultrasonic Sensor Example for QNX.
 *
 * This example demonstrates how to interface with the HC‑SR04 ultrasonic sensor
 * on a QNX system. It sends a trigger pulse, waits for the echo’s rising and falling
 * edges, and then calculates the distance based on the pulse duration.
 *
-* Wiring on the Trilobot:
+* Wiring:
 *   - P1: 5V
 *   - P2: US_TRIG   -> connected to GPIO 13
 *   - P3: US_ECHO   -> connected to GPIO 25 (with built‑in resistor divider)
--- hc-sr04_ultrasonic_sensor/python/Ultrasonic.py
+++ hc-sr04_ultrasonic_sensor/python/Ultrasonic.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
 """
+Copyright (c) 2025, BlackBerry Limited. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 Ultrasonic Sensor Module for QNX using rpi_gpio.
  
 This module demonstrates how to interface with the HC‑SR04 ultrasonic sensor on a QNX system.
 It sends a trigger pulse, waits for the echo’s rising and falling edges, and calculates the distance
 based on the duration of the echo pulse.
  
-Wiring on the Trilobot:
+Wiring:
   - VCC: 5V
   - Trigger (US_TRIG): connected to GPIO 13
   - Echo (US_ECHO): connected to GPIO 25
